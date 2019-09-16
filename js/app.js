2e0e04085515f3c394664310f6af24a7bef41cd1
Abraham Silva <abraham@tenatek.com>
2019-09-15 23:58:33 -0500

    side-bar completed

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
   $('#sidebarCollapse').on('click', function() {
-    $('#sidebar').toggleClass('active');
-    $('#sidebarCollapse').toggleClass('super');
+    $('#sidebar').removeClass('reset-margin');
+    $('#sidebarCollapse').attr('data-toggle', 'false');
+    $('#sidebarShow').addClass('enable');
+    $('#sidebar').addClass('hide-nav');
+    $('#content').addClass('normalize-padding');
+  });
+  $('#sidebarShow').on('click', function() {
+    if ($('#sidebarCollapse').attr('data-toggle') == 'false') {
+      $('#content').removeClass('normalize-padding');
+      $('#sidebar').removeClass('hide-nav');
+      $('#sidebarShow').removeClass('enable');
+      if ($(window).width() <= 768) {
+        $('#sidebar').addClass('reset-margin');
+      }
+    } else {
+      $('#sidebar').addClass('reset-margin');
+      $('#sidebarShow').removeClass('enable');
+    }
   });
 });
